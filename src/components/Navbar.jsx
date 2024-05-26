f8e8e12949a6f336a17675e14075601720a4152f
pauval <pauly.val@gmail.com>
2024-05-26 06:39:34 -0700

    Detalles imagenes e iconos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaUser } from 'react-icons/fa';
+import { FaHome, FaUser, FaBirthdayCake } from 'react-icons/fa';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-link">
-        <FaHome /> Home
-      </Link>
-      <Link to="/contact" className="nav-link">
-        <FaUser /> Contacto
-      </Link>
+      <div className="nav-links">
+        <Link to="/" className="nav-link">
+          <FaHome /> Home
+        </Link>
+        <Link to="/contact" className="nav-link">
+          <FaUser /> Contacto
+        </Link>
+      </div>
+      <div className="nav-brand">
+        HappyCake < FaBirthdayCake /> 
+      </div>
     </nav>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import cakeImage from '../assets/cake.jpg';
+import cakeImage from '../assets/cake.png';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Bienvenido a HappyCake</h1>
-      <h3>Lugar de los pasteles felices</h3>
+      <h4>Lugar de los pasteles felices</h4>
       <img src={cakeImage} alt="Cake" className="cake-image" />
     </div>
   );
